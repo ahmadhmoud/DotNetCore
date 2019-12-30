e5f5e5dfb0b1159913bef3f2b13905ff03d6d3d3
Ahmad <a-ahhmou@microsoft.com>
2019-12-30 17:26:12 +0200

    update 1

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -19,6 +19,7 @@ namespace WebApplication
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
                 .UseStartup<Startup>();
     }
 }
